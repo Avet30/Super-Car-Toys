da84440d44187015f61637ab34ff856d0112b759
Avet <Avet@DESKTOP-42M9SLF>
2022-07-27 14:32:39 +0200

    v2Update v1 Ajoutez des fichiers projet.

--- AuxiPress.Models/ViewModels/ShoppingCartViewModel.cs
+++ AuxiPress.Models/ViewModels/ShoppingCartViewModel.cs
@@ -9,5 +9,6 @@ namespace AuxiPress.Models.ViewModels
     public class ShoppingCartViewModel
     {
         public IEnumerable<ShoppingCart> ListCart { get; set; }
+        public double CartTotal { get; set; }
     }
 }
--- AuxiPress_Project/Areas/Customer/Controllers/CartController.cs
+++ AuxiPress_Project/Areas/Customer/Controllers/CartController.cs
@@ -11,6 +11,7 @@ namespace AuxiPress_Project.Areas.Customer.Controllers
     public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+        public int OrderTotal { get; set; }
         public ShoppingCartViewModel ShoppingCartViewModel { get; set; }
 
         public CartController(IUnitOfWork unitOfWork)
@@ -27,7 +28,76 @@ namespace AuxiPress_Project.Areas.Customer.Controllers
             {
                 ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
             };
+            foreach(var cart in ShoppingCartViewModel.ListCart)
+            {
+                cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+                ShoppingCartViewModel.CartTotal+=(cart.Price * cart.Count);
+            }
             return View(ShoppingCartViewModel);
+        }
+
+        public IActionResult Summary()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //ShoppingCartViewModel = new ShoppingCartViewModel()
+            //{
+            //    ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProperties: "Product")
+            //};
+            //foreach (var cart in ShoppingCartViewModel.ListCart)
+            //{
+            //    cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price, cart.Product.Price50, cart.Product.Price100);
+            //    ShoppingCartViewModel.CartTotal += (cart.Price * cart.Count);
+            //}
+            //return View(ShoppingCartViewModel);
+            return View();
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u=> u.Id == cartId);
+            _unitOfWork.ShoppingCart.IncrementCount(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Minus(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            if(cart.Count <= 1)
+            {
+                _unitOfWork.ShoppingCart.Remove(cart);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.DecrementCount(cart, 1);
+            }
+            
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Remove(int cartId)
+        {
+            var cart = _unitOfWork.ShoppingCart.GetFirstOrDefault(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
+        {
+            if(quantity<=50)
+            {
+                return price;
+            }
+            else
+            {
+                if(quantity<=100)
+                {
+                    return price50;
+                }
+                return price100;
+            }
         }
     }
 }
